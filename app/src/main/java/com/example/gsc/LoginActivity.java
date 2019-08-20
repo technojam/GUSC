623a5ee47ef622bce8a2fa9329fa78be5f1e51a6
Leo5661 <singhra66@gmail.com>
2019-08-20 21:05:47 +0530

    home added

--- app/src/main/java/com/example/gsc/LoginActivity.java
+++ app/src/main/java/com/example/gsc/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.gsc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button mLogInBtn = findViewById(R.id.LoginBtn);
+        TextView mSignUpBtn = findViewById(R.id.SignUpBtn);
+
+        mLogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
